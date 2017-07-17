18e6bab134684dd8c02f6d6f248513f7fd47ce51
Muthu <muthukumaraswamy.b@2adpro.com>
2017-07-18 05:10:20 +0530

    files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# react-example
+# react-exapme
+A new React project using only webpack
+
+## Run locally
+```
+npm install
+npm run dist
+open dist/index.html
+```
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const path = require('path');
+const config = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+};
+module.exports = config;
